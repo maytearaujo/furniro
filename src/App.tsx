8d0f999f23fd68e6c4375b8d9e996b76f52694d6
Maytê <maytearaujo@yahoo.com.br>
2024-06-10 23:30:03 -0300

    feat: styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
+import GlobalStyles from "./styles/global"
 
 function App() {
 
   return (
     <>
+      <GlobalStyles />
       <h1 className="text-3xl font-bold underline text-red-300 text-center">
         Hello world!<br />
         Welcome to the Furniro

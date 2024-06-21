6ee262bd0a21b525e45d5a3a4ae7ab188e2bf260
Maytê <maytearaujo@yahoo.com.br>
2024-06-21 17:14:11 -0300

    Ajustes finais

--- src/components/paginationComponent/PaginationComponent.tsx
+++ src/components/paginationComponent/PaginationComponent.tsx
@@ -3,7 +3,7 @@ const PaginationComponent = ({setCurrentPage, pages}) => {
     return (
         <div>
             {Array.from(Array(pages), (_item, index) => {
-                return <button value={index} onClick={(e: React.ChangeEvent<HTMLInputElement>) => setCurrentPage(Number(e.target.value))}>{index + 1}</button>
+                return <button value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index + 1}</button>
             })}
         </div>
     )

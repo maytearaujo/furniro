14ef77d5be512fc330cebe6800e5e645636dd5a9
Maytê <maytearaujo@yahoo.com.br>
2024-07-07 22:10:31 -0300

    feat mosaic

--- src/components/FuniroFurniture/FuniroFurniture.tsx
+++ src/components/FuniroFurniture/FuniroFurniture.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 import axios from 'axios';
-import 'swiper/css';
+// import 'swiper/css';
 import * as S from './FuniroFurnitureStyles'
 
 const FuniroFurniture = () => {
@@ -19,7 +19,7 @@ const FuniroFurniture = () => {
         console.error(error);
       }
     };
-  
+
     fetchProducts()
   }, [])
 
@@ -27,33 +27,41 @@ const FuniroFurniture = () => {
     <S.FuniroFurniture>
       <h2>Funiro Furniture</h2>
 
-      <S.SlideItem>
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3.8}
-          draggable={true}
-          breakpoints={{
-            1600: { slidesPerView: 3.8 },
-            1400: { slidesPerView: 3.25 },
-            1200: { slidesPerView: 3 },
-            1024: { slidesPerView: 2.5 },
-            768: { slidesPerView: 2, spaceBetween: 30 },
-            576: { slidesPerView: 1.5, spaceBetween: 20 },
-            375: { slidesPerView: 1.25, spaceBetween: 15 },
-            320: { slidesPerView: 1, spaceBetween: 10 },
-          }}
-        >
-          {products.map((item) => (
-            <SwiperSlide key={item.id}>
+      <S.Mosaic>
+        <S.Group1>
+          {products.slice(0, 4).map((item) => (
+            <div key={item.id}>
               <img
                 src={item.imgUrl}
                 alt="Furniture"
               />
-            </SwiperSlide>
+            </div>
           ))
           }
-        </Swiper>
-      </S.SlideItem>
+        </S.Group1>
+        <S.Group2>
+          {products.slice(4,5).map((item) => (
+            <div key={item.id}>
+              <img
+                src={item.imgUrl}
+                alt="Furniture"
+              />
+            </div>
+          ))
+          }
+        </S.Group2>
+        <S.Group3>
+          {products.slice(5, 9).map((item) => (
+            <div key={item.id}>
+              <img
+                src={item.imgUrl}
+                alt="Furniture"
+              />
+            </div>
+          ))
+          }
+        </S.Group3>
+      </S.Mosaic>
     </S.FuniroFurniture>
   )
 }
--- src/components/FuniroFurniture/FuniroFurnitureStyles.tsx
+++ src/components/FuniroFurniture/FuniroFurnitureStyles.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const FuniroFurniture = styled.main`
-  height: 100vh;
+  /* border: solid green; */
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
 
   h2{
@@ -14,12 +15,35 @@ export const FuniroFurniture = styled.main`
   }
 `
 
-export const SlideItem = styled.div`
-width: 70%;
+export const Mosaic = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: .5rem;
+/* display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-gap: 1rem;
+width: 70%; */
 
-  img {
+
+`
+
+export const Group1 = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr ;
+  grid-gap: .1rem;
+  `
+export const Group2 = styled.div`
+/* height: 80%; */
+    /* img {
     width: 100%;
     height: 300px;
     object-fit: cover;
-  }
+  } */
+  `
+export const Group3 = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr ;
+  grid-gap: .1rem;
 `

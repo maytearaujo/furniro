fbb69ef61284aca0e5d0b7e1103cf6d7d0e8ca8a
Maytê <maytearaujo@yahoo.com.br>
2024-10-23 09:07:38 -0300

    fixed: ErrorPage

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App.jsx";
-import ErrorPage from "../page/errorPage/ErrorPage.jsx";
+import ErrorPage from "../page/errorPage/ErrorPage.jsx"
 import Login from "../components/Login/Login.jsx";
 import Home from "../page/home/Home.jsx";
 import Signup from "../components/Signup/Signup.jsx";

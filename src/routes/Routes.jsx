f63e0dc856690a6bf7f809257f06bf15a4f96153
Maytê <maytearaujo@yahoo.com.br>
2024-10-25 22:41:00 -0300

    Products

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,14 +5,14 @@ import Home from "../page/home/Home.jsx";
 import MyErrorPage from "../page/errorPage/ErrorPage.jsx"
 import Login from "../components/Login/Login.jsx";
 import Signup from "../components/Signup/Signup.jsx";
-// import Shop from "../page/shop/Shop.jsx";
+import Shop from "../page/shop/Shop.jsx";
 
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute.jsx";
 import Dashboard from "../components/Dashboard/Dashboard.jsx";
 import Cart from "../components/cart/Cart.jsx";
 import Contact from "../page/contact/Contact.jsx";
 
-// const api = 'https://furniro-lake.vercel.app/Products'
+const api = 'https://furniro-json-git-main-mayte-araujos-projects.vercel.app/Products'
 
 const router = createBrowserRouter([
     {
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
             { path: '/', element: <Home /> },
             { path: '/login', element: <Login /> },
             { path: '/signup', element: <Signup /> },
-            // { path: '/shop', element: <Shop api={api} /> },
+            { path: '/shop', element: <Shop api={api} /> },
             { path: '/contact', element: <Contact /> },
             {
                 path: '/dashboard',
